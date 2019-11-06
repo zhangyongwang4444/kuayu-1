fbfc6dc02f8e67f73ba315e183a34f1a3aee1856
zhangyongwang <20225614@qq.com>
2019-11-06 19:14:57 +0800

    封装JSONP

--- frank-com/public/frank.js
+++ frank-com/public/frank.js
@@ -1,21 +1,34 @@
-
-
-
-
-
-
-
-
-const random = 'frankJSONPCallbackName' + Math.random()
-window[random] = (data) => {
-    console.log(data)
+function jsonp(url) {
+    return new Promise((resolve, reject) => {
+        const random = 'frankJSONPCallbackName' + Math.random()
+        window[random] = (data) => {
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?functionName=${random}`
+        script.onload = () => {
+            script.remove()
+        }
+        script.onerror = () => {
+            reject()
+        }
+        document.body.appendChild(script)
+    })
 }
-const script = document.createElement('script')
-script.src = `http://qq.com:8888/friends.js?functionName=${random}`
 
-script.onload = () => {
-    script.remove()
-}
 
-document.body.appendChild(script)
+
+jsonp('http://qq.com:8888/friends.js')
+    .then((data) => {
+        console.log(data)
+    })
+
+
+
+
+
+
+
+
+
 
